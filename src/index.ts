f3353bf7f497cf5ed2f4c9347034ce955f49c9b8
niravkarathiya <niravk.albiorix@gmail.com>
2025-01-30 14:27:37 +0530

    added comments

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,28 @@
+// environment access configuration
 import dotenv from 'dotenv';
 dotenv.config();
 
+//libraries imports
 import express, { Request, Response } from 'express';
 import helmet from 'helmet';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import { engine } from 'express-handlebars';
 import path from 'path';
+
+//controller imports
 import authRoute from './controllers/auth/auth.route';
 import productRoute from './controllers/product/product.route';
 import reviewRoute from './controllers/reviews/reviews.route';
 import purchaseRoute from './controllers/purchase/purchase.routes';
 import addressRoute from './controllers/address/address.routes';
+
+//database connection
 import connectDB from './dbConnection';
 
 const app = express();
 
+//set view engine for the handlebars and configuration
 app.engine("handlebars", engine({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 app.set("views", path.join(__dirname, "./templates"));
@@ -37,12 +44,12 @@ app.use('/review', reviewRoute);
 app.use('/purchase', purchaseRoute);
 app.use('/address', addressRoute);
 
-// Routes
+// starter page 
 app.get('/', (req: Request, res: Response) => {
     res.json({ message: 'Hello there, Greeting from the server!' });
 });
 
-// Start server
+// Start server and port setup
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
     console.log(`App running on PORT: ${PORT}`);
